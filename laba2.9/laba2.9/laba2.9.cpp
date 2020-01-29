2489f7de791c94a055d928dfee96ebb3460dc753
Artsiom <solovyev768@gmail.com>
2020-01-29 22:45:06 +0300

    patch 1

---
+++ laba2.9/laba2.9/laba2.9.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include<math.h>
+using namespace std;
+double fan(int n, int a)
+{
+	if (a == n)
+		return pow(n, 1 / 2.);
+	return pow(a + fan(n, a + 1), 1 / 2.);
+}
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	double n, x, a = 1;
+	cout << "Введите n : ";
+	cin >> n;
+	cout << "Значение рекурсивной функции : " << fan(n, a) << endl;
+	x = pow(n, 1 / 2.);
+	a = n - 1;
+	for (int i = 0; i < n - 1; i++)
+	{
+		x = pow(a + x, 1 / 2.);
+		a--;
+	}
+	cout << "Значение обычной функции : " << x;
+}
