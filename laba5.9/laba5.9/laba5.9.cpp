6a1f924f7858f577475675e43ae163c7f9187702
Artsiom <solovyev768@gmail.com>
2020-02-22 23:46:58 +0300

    ;)

---
+++ laba5.9/laba5.9/laba5.9.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+struct set {
+    int info;
+    set* next;
+}*head;
+set* push(set* p, int a) {
+    set* t = new set;
+    t->info = a;
+    t->next = p;
+    return t;
+}
+void fun(set* p, int* out) {
+    set* MIN = NULL, * MAX = NULL, * t;
+    int min = 100, max = 0, a = 0, a1, a2, z=0;
+    for (; p!=NULL; p = p->next) {
+        a++;
+        if (min > p->info) {
+            min = p->info;
+            MIN = p;
+            a1 = a;
+        } 
+    }
+    p = head;
+    a = 0;
+    for (p; p != NULL; p = p->next) {
+        a++;
+        if (max < p->info) {
+            max = p->info;
+            MAX = p;
+            a2 = a;
+        }  
+    }
+    if (a1 > a2) {
+        auto k = MIN;
+        MIN = MAX;
+        MAX = k;
+    }
+    p = MIN->next;
+    while (p != MAX) {
+        z++;
+        t = p;
+        p = p->next;
+        MIN->next = p;
+        delete t;
+    }
+    *out = z;
+}
+void view(set* p) {
+    while (p != NULL) {
+        cout << p->info << " ";
+        p = p->next;
+    }
+}
+int main(){
+    setlocale(LC_ALL, "ru");
+    srand(time(NULL));
+    int a, x;
+    cout << "Введете количество элементов в стеке : ";
+    cin >> x;
+    for (int i = 0; i < x; i++) {
+        a = rand()%51;
+        head = push(head, a);
+    }
+    view(head);
+    fun(head,&a);
+    cout << endl << "Удалено " << a << " элеметов" << endl;
+    view(head);
+}
---
+++ laba6.9/laba6.9/laba6.9.cpp
@@ -0,0 +1,111 @@
+﻿#include <iostream>
+using namespace std;
+struct set {
+    int info;
+    set* next, * prev;
+}*head,*tail;
+void end() {
+    exit(0);
+}
+set* push_head(set* p, int x) {
+    set* t = new set;
+    if (head == NULL) {
+        t->next = t->prev = NULL;
+        t->info = x;
+        head = tail = t;
+    }
+    else {
+        t->prev = NULL;
+        t->next = p;
+        t->info = x;
+        p->prev = t;
+    }
+    return t;
+}
+set* push_tail(set* p, int x) {
+    set* t = new set;
+    if (tail == NULL) {
+        t->next = t->prev = NULL;
+        t->info = x;
+        head = tail = t;
+    }
+    else {
+        t->next = NULL;
+        t->prev = p;
+        t->info = x;
+        p->next = t;
+    }
+    return t;
+}
+void del(set* p, int* out) {
+    set* MIN = NULL, * MAX = NULL, * t;
+    int min = 100, max = 0, a = 0, a1, a2, z = 0;
+    for (; p != NULL; p = p->next) {
+        a++;
+        if (min > p->info) {
+            min = p->info;
+            MIN = p;
+            a1 = a;
+        }
+    }
+    p = head;
+    a = 0;
+    for (p; p != NULL; p = p->next) {
+        a++;
+        if (max < p->info) {
+            max = p->info;
+            MAX = p;
+            a2 = a;
+        }
+    }
+    if (a1 > a2) {
+        auto k = MIN;
+        MIN = MAX;
+        MAX = k;
+    }
+    p = MIN->next;
+    while (p != MAX) {
+        z++;
+        t = p;
+        p = p->next;
+        MIN->next = p;
+        p->prev = MIN;
+        delete t;
+    }
+    *out = z;
+}
+void view_head(set* p) {
+    while (p != NULL) {
+        cout << p->info << " ";
+        p = p->next;
+    }
+    cout << endl;
+}
+void view_tail(set* p) {
+    while (p != NULL) {
+        cout << p->info << " ";
+        p = p->prev;
+    }
+    cout << endl;
+}
+void logics() {
+    char fun;
+    cout << "Введите нужную букву : ";
+    cin >> fun;
+    int x, a;
+    switch (fun) {
+    case 'a':view_head(head); break;
+    case 'b':view_tail(tail); break;
+    case 'c':cout << "Введите число : "; cin >> x; head = push_head(head, x); break;
+    case 'd':cout << "Введите число : "; cin >> x; tail = push_tail(tail, x); break;
+    case 'e':del(head, &a); cout << endl << "Удалено " << a << " элеметов" << endl; break;
+    case 'x':end();
+    }
+    logics();
+}
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    cout << "Просмотр с начала : a\nПросмотр с конца : b\nДобавление в начало : c\nДобавление в конец : d\nУдаление элементов : e\nЗавершение работы : x\n";
+    logics();
+}
